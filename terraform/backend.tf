3ef0003db2f19c7f942aa5ed1577b1c94fa8ec64
Naman Garg <omsai16019091@gmail.com>
2026-07-24 10:48:15 +0530

    Fixing Issue related to terraform State S3 Bucket

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "terraformstate20072026"
+    bucket       = "terraformstate24072026"
     key          = "eks/dev/terraform.tfstate"
     region       = "us-east-1"
     use_lockfile = true
